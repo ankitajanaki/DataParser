04be334ed89506fe6de834f5b1c4b4c01c54eee9
ankitajanaki <ankita.janaki01@gmail.com>
2019-02-28 23:02:30 -0800

    add test of file reading to main method

--- src/Main.java
+++ src/Main.java
@@ -5,4 +5,9 @@
 
 
 public class Main {
+    public static void main(String[] args) {
+        //Test of utils
+        String data=Utils.readFileAsString("data/2016_Presidential_Results.csv");
+        System.out.println(data);
+    }
 }
